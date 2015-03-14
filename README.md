85caa2853319a9a4f200be1497b81c0fae3bb25b
janovak <janovak@mtu.edu>
2015-03-14 00:48:24 -0400

    Add backtracking and README

---
+++ README.md
@@ -0,0 +1,3 @@
+Solves a Sudoku puzzle by first trying to use logical method to fill in as
+many squares as possible and then switching to a backtracking algorithm to
+solve any remaining squares.
--- sudoku.py
+++ sudoku.py
@@ -56,16 +56,23 @@ class Sudoku:
 
     # Iteratively solve the board from its initial state
     def solve(self):
+        # Used to check if we cannot be certain of the next move
+        firstUnsolved = self.openList[-1]
         # Iterate over the open list until it is empty
         while len(self.openList) > 0:
+            # Cannot solve any more squares without trial and error
+            if firstUnsolved == self.openList[0] and len(self.openList) != 1:
+                break
+
             # Used to check if a square was solved this iteration
             oldLength = len(self.openList)
             possibilities = self.getPossibilities(self.openList[0])
             i, j, k, l = self.openList[0]
             if len(possibilities) == 1:
+                firstUnsolved = self.openList[-1]
                 # If the first square in the open list only has one possible solution
                 # given the board's current state
-                self.found(i, j, k, l, possibilities[0])
+                self.found(self.openList[0], possibilities[0])
             else:
                 # If a row, column, or box only has one possible square for a
                 # value then put the value in that square
@@ -78,7 +85,8 @@ class Sudoku:
                         if self.board[i][b][k][d].a == 0 and (j, l) != (b, d):
                             answer = list(set(answer) - set(self.getPossibilities((i, b, k, d))))
                 if len(answer) == 1:
-                    self.found(i, j, k, l, answer[0])
+                    firstUnsolved = self.openList[-1]
+                    self.found(self.openList[0], answer[0])
                     continue
 
                 # Check if none of the other squares in the column could hold any
@@ -89,7 +97,8 @@ class Sudoku:
                         if self.board[a][j][c][l].a == 0 and (i, k) != (a, c):
                             answer = list(set(answer) - set(self.getPossibilities((a, j, c, l))))
                 if len(answer) ==  1:
-                    self.found(i, j, k, l, answer[0])
+                    firstUnsolved = self.openList[-1]
+                    self.found(self.openList[0], answer[0])
                     continue
 
                 # Check if none of the other squares in the box could hold any
@@ -100,25 +109,54 @@ class Sudoku:
                         if self.board[i][j][c][d].a == 0:
                             answer = list(set(answer) - set(self.getPossibilities((i, j, c, d))))
                 if len(answer) == 1:
-                    self.found(i, j, k, l, answer[0])
+                    firstUnsolved = self.openList[-1]
+                    self.found(self.openList[0], answer[0])
                     continue
 
             # Move first element to the end if first element was not removed
             if oldLength == len(self.openList):
                 self.openList.append(self.openList.pop(0))
 
+        # Logical methods could not solve every square
+        if len(self.openList) > 0:
+            self.depthFirst()
+
+    # Use backtracking to solve the puzzle from its current state
+    def depthFirst(self):
+        if len(self.openList) == 0:
+            return True
+
+        curSquare = self.openList[0]
+        possibilities = self.getPossibilities(curSquare)
+
+        i, j, k, l = curSquare
+        for e in possibilities:
+            self.found(curSquare, e)
+            if self.depthFirst():
+                return True
+            # Restore to state of the last partial, possibile solution
+            self.board[i][j][k][l].a = 0
+            self.rows[self.board[i][j][k][l].r].append(e)
+            self.cols[self.board[i][j][k][l].c].append(e)
+            self.boxes[self.board[i][j][k][l].b].append(e)
+            self.openList.insert(0, curSquare)
+
+        return False
+
     # Set the square the value found and remove that value from the square's
     # row, column, and box and remove the square from the open list
-    def found(self, i, j, k, l, el):
-        self.board[i][j][k][l].a = el
-        self.removeFound(i, j, k, l, el)
-        del self.openList[0]
+    def found(self, index, e):
+        i, j, k, l = index
+        self.board[i][j][k][l].a = e
+        self.removeFound(index, e)
+        self.openList.remove(index)
 
     # Remove the value from the square's row, column, and box
-    def removeFound(self, i, j, k, l, el):
-        safeRemove(self.rows[self.board[i][j][k][l].r], el)
-        safeRemove(self.cols[self.board[i][j][k][l].c], el)
-        safeRemove(self.boxes[self.board[i][j][k][l].b], el)
+    def removeFound(self, index, e):
+        i, j, k, l = index
+        self.rows[self.board[i][j][k][l].r].remove(e)
+        self.cols[self.board[i][j][k][l].c].remove(e)
+        self.boxes[self.board[i][j][k][l].b].remove(e)
 
     # Returns a list of all possible values for the square
     def getPossibilities(self, index):
@@ -140,7 +178,7 @@ class Sudoku:
     def col(self, i, j, k, l):
         return j * 3 + l
 
-    # Prints the board with 0s for unsolved squares
+    # Prints the board
     def printBoard(self):
         for i in range(3):
             for j in range(3):
